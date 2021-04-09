9173a4ae9293b2ef17476de86bae45a7967b6f47
Ghozti <ybalderas1005@gmail.com>
2021-04-09 13:34:52 -0400

    - added getters in Student

--- src/ghozti/schoolDBsystem/student/Student.java
+++ src/ghozti/schoolDBsystem/student/Student.java
@@ -6,4 +6,20 @@ public class Student {
     private int age;
     private String birthDate;
     private int GPA;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getBirthDate(){
+        return birthDate;
+    }
+
+    public int getGPA(){
+        return GPA;
+    }
 }
