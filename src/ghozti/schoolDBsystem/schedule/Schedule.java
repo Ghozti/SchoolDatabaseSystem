fe1351be13b87949a6f1cf7a1e6fda901be11a0d
Ghozti <ybalderas1005@gmail.com>
2021-04-09 14:21:47 -0400

    - added getters - added fields - added constructors - in Schedule

--- src/ghozti/schoolDBsystem/schedule/Schedule.java
+++ src/ghozti/schoolDBsystem/schedule/Schedule.java
@@ -1,4 +1,36 @@
 package ghozti.schoolDBsystem.schedule;
 
+import ghozti.schoolDBsystem.student.Student;
+
 public class Schedule {
+
+    private String[] classes = new String[7];
+    private Student scheduleStudent;
+    private String studentName = scheduleStudent.getName();
+    private String ID;
+
+    public Schedule(Student student){
+        scheduleStudent = student;
+    }
+
+    public Schedule(Student student, String ID){
+        scheduleStudent = student;
+        this.ID = ID;
+    }
+
+    public String[] getClasses(){
+        return classes;
+    }
+
+    private Student getStudent(){
+        return scheduleStudent;
+    }
+
+    private String getStudentName(){
+        return studentName;
+    }
+
+    public String getID(){
+        return ID;
+    }
 }
