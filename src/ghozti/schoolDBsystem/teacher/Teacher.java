b5384c863df6d824dd7637c2510936da106beea7
Ghozti <ybalderas1005@gmail.com>
2021-04-09 13:39:49 -0400

    - added fields and getters to Teacher

--- src/ghozti/schoolDBsystem/teacher/Teacher.java
+++ src/ghozti/schoolDBsystem/teacher/Teacher.java
@@ -1,4 +1,25 @@
 package ghozti.schoolDBsystem.teacher;
 
 public class Teacher {
+
+    private String name;
+    private int age;
+    private String birthDate;
+    private String subject;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getBirthDate(){
+        return birthDate;
+    }
+
+    public String getSubject(){
+        return subject;
+    }
 }
