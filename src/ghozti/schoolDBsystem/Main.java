61de158ffb37b9228ea7b948989bd322ffa76f07
Ghozti <ybalderas1005@gmail.com>
2021-04-11 12:21:25 -0400

    - "finished" the sorter

--- src/ghozti/schoolDBsystem/Main.java
+++ src/ghozti/schoolDBsystem/Main.java
@@ -1,8 +1,18 @@
 package ghozti.schoolDBsystem;
 
+import ghozti.schoolDBsystem.student.Student;
+import ghozti.schoolDBsystem.utils.AlphabeticalSorter;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        AlphabeticalSorter alphabeticalSorter = new AlphabeticalSorter();
+        ArrayList<Student> list = new ArrayList<>();
+        for (int i = 1; i < 26; i++) {
+            list.add(new Student());
+        }
+        alphabeticalSorter.sort();
     }
 }
--- src/ghozti/schoolDBsystem/utils/AlphabeticalSorter.java
+++ src/ghozti/schoolDBsystem/utils/AlphabeticalSorter.java
@@ -6,9 +6,62 @@ import java.util.ArrayList;
 public class AlphabeticalSorter {
 
     public ArrayList<Student> sort(ArrayList<Student> list){
-        ArrayList<Student> nList;
+        ArrayList<Student> nList = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
-
+            if (list.get(i).getName().toLowerCase().charAt(0) == 'a'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'b'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'c'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'd'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'e'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'f'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'g'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'h'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'i'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'j'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'k'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'l'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'm'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'n'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'o'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'p'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'q'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'r'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 's'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 't'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'u'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'v'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'w'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'x'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'y'){
+                nList.add(list.get(i));
+            }else if (list.get(i).getName().toLowerCase().charAt(0) == 'z'){
+                nList.add(list.get(i));
+            }
+            //TODO make this more efficient lol
         }
         return nList;
     }
