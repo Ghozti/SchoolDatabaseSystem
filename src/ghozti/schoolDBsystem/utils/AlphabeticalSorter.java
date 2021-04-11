d769842f6b8a7e8d236b4c1f39d75438a1809bab
Ghozti <ybalderas1005@gmail.com>
2021-04-11 12:05:29 -0400

    - started to work on the AlphabeticalSorter

--- src/ghozti/schoolDBsystem/utils/AlphabeticalSorter.java
+++ src/ghozti/schoolDBsystem/utils/AlphabeticalSorter.java
@@ -1,4 +1,15 @@
 package ghozti.schoolDBsystem.utils;
 
+import ghozti.schoolDBsystem.student.Student;
+import java.util.ArrayList;
+
 public class AlphabeticalSorter {
+
+    public ArrayList<Student> sort(ArrayList<Student> list){
+        ArrayList<Student> nList;
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+        return nList;
+    }
 }
