0525f9ec48e2d7d4a0dfb16085ac6dc49c6ea3a8
Ghozti <ybalderas1005@gmail.com>
2021-04-12 15:52:25 -0400

    - "finished" the RemoveByName algorithm

--- src/ghozti/schoolDBsystem/utils/RemoveByName.java
+++ src/ghozti/schoolDBsystem/utils/RemoveByName.java
@@ -1,4 +1,16 @@
 package ghozti.schoolDBsystem.utils;
 
+import ghozti.schoolDBsystem.student.Student;
+import java.util.ArrayList;
+
 public class RemoveByName {
+
+    public void removeByName(ArrayList<Student> list, String target){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getName().equals(target)){
+                list.remove(i);
+                return;
+            }
+        }
+    }
 }
