36e878fb2f2c308db5809dca22aca7c08a70139e
Ghozti <ybalderas1005@gmail.com>
2021-04-12 16:05:30 -0400

    - commented on Student

--- src/ghozti/schoolDBsystem/Main.java
+++ src/ghozti/schoolDBsystem/Main.java
@@ -11,11 +11,6 @@ public class Main {
     public static void main(String[] args) {
         AlphabeticalSorter alphabeticalSorter = new AlphabeticalSorter();
         ArrayList<Student> list = new ArrayList<>();
-        /*
-        for (int i = 122; i > 96; i--) {
-            list.add(new Student((char) i,15,"10/29/2005"));
-        }
-         */
 
         list.add(new Student("yesi",15,"20"));
         list.add(new Student("albert",15,"20"));
--- src/ghozti/schoolDBsystem/student/Student.java
+++ src/ghozti/schoolDBsystem/student/Student.java
@@ -2,37 +2,43 @@ package ghozti.schoolDBsystem.student;
 
 public class Student {
 
-    private String name;
-    private int age;
-    private String birthDate;
-    private int GPA;
+    private String name;//name of student
+    private int age;//age of student
+    private String birthDate;//birthdate of student
+    private int GPA;//GPA of student
 
-    public String getName(){
-        return name;
-    }
-
-    public int getAge(){
-        return age;
-    }
-
-    public String getBirthDate(){
-        return birthDate;
-    }
-
-    public int getGPA(){
-        return GPA;
-    }
-
+    //constructor #1 will allow the user to set the fields except GPA
     public Student(String name,int age, String birthDate){
         this.name = name;
         this.age = age;
         this.birthDate = birthDate;
     }
 
+    //constructor #2 will allow the user to set all fields
     public Student(String name,int age, String birthDate,int GPA){
         this.name = name;
         this.age = age;
         this.birthDate = birthDate;
         this.GPA = GPA;
+    }
+
+    //name getter
+    public String getName(){
+        return name;
+    }
+
+    //age getter
+    public int getAge(){
+        return age;
+    }
+
+    //birthdate getter
+    public String getBirthDate(){
+        return birthDate;
+    }
+
+    //GPA getter
+    public int getGPA(){
+        return GPA;
     }
 }
