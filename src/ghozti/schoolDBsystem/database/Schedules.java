6d59b89154bbc41661541335a3cc6c3ec550a7e9
Ghozti <ybalderas1005@gmail.com>
2021-04-13 11:12:04 -0400

    - filled in Schedules in database

--- src/ghozti/schoolDBsystem/database/Schedules.java
+++ src/ghozti/schoolDBsystem/database/Schedules.java
@@ -1,4 +1,13 @@
 package ghozti.schoolDBsystem.database;
 
+import ghozti.schoolDBsystem.schedule.Schedule;
+import java.util.ArrayList;
+
 public class Schedules {
+
+    private ArrayList<Schedule> schedules = new ArrayList<>();
+
+    public ArrayList<Schedule> getSchedules(){
+        return schedules;
+    }
 }
