6d58a9b779e018a6c7100520c9001466a4c75b12
Ghozti <ybalderas1005@gmail.com>
2021-04-13 10:58:53 -0400

    - almost finished createClass - filled in Classes in database

--- src/ghozti/schoolDBsystem/UI/Options.java
+++ src/ghozti/schoolDBsystem/UI/Options.java
@@ -1,8 +1,30 @@
 package ghozti.schoolDBsystem.UI;
 
+import ghozti.schoolDBsystem.teacher.Teacher;
+
+import java.util.Scanner;
+
 public class Options {
 
     public void createClass(){
+        Scanner scanner = new Scanner(System.in);
 
+        String classname;
+        String ID;
+        Teacher teacher;
+        int period;
+
+        System.out.println();//TODO add dialogue constants (class name)
+        classname = scanner.nextLine();//sets the class name
+
+        System.out.println();//TODO add dialogue constants (ID)
+        ID = scanner.nextLine();//sets class ID
+
+        //TODO call the create teacher function
+
+        System.out.println();//TODO add dialogue constants (period)
+        period = scanner.nextInt();//sets class period
+
+        Classes
     }
 }
--- src/ghozti/schoolDBsystem/database/Classes.java
+++ src/ghozti/schoolDBsystem/database/Classes.java
@@ -1,4 +1,12 @@
 package ghozti.schoolDBsystem.database;
 
+import java.util.ArrayList;
+
 public class Classes {
+
+    private ArrayList<Class> classes = new ArrayList<>();
+
+    public ArrayList<Class> getClasses(){
+        return classes;
+    }
 }
