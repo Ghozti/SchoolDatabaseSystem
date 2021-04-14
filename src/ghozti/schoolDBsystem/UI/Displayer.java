d349062a28003530a8fd5a2b3603fd1a372cd3a2
Ghozti <ybalderas1005@gmail.com>
2021-04-14 15:58:03 -0400

    - added display methods in Displayer

--- src/ghozti/schoolDBsystem/UI/Displayer.java
+++ src/ghozti/schoolDBsystem/UI/Displayer.java
@@ -1,4 +1,25 @@
 package ghozti.schoolDBsystem.UI;
 
+import ghozti.schoolDBsystem.classes.Class;
+import ghozti.schoolDBsystem.schedule.Schedule;
+import ghozti.schoolDBsystem.student.Student;
+import ghozti.schoolDBsystem.teacher.Teacher;
+
 public class Displayer {
+
+    public void displayStudent(Student student){
+
+    }
+
+    public void displayTeacher(Teacher teacher){
+
+    }
+
+    public void displaySchedule(Schedule schedule){
+
+    }
+
+    public void displayClass(Class clas$){
+
+    }
 }
--- src/ghozti/schoolDBsystem/database/Teachers.java
+++ src/ghozti/schoolDBsystem/database/Teachers.java
@@ -1,7 +1,6 @@
 package ghozti.schoolDBsystem.database;
 
 import ghozti.schoolDBsystem.teacher.Teacher;
-
 import java.util.ArrayList;
 
 public class Teachers {
