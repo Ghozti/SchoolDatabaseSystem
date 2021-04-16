59938633d9a65f7a720cc9de3c8fab6d6dc09f6b
Ghozti <ybalderas1005@gmail.com>
2021-04-16 14:05:36 -0400

    - added the other RemoveByName methods in RemoveByName

--- src/ghozti/schoolDBsystem/utils/RemoveByName.java
+++ src/ghozti/schoolDBsystem/utils/RemoveByName.java
@@ -1,13 +1,44 @@
 package ghozti.schoolDBsystem.utils;
 
+import ghozti.schoolDBsystem.classes.Class;
+import ghozti.schoolDBsystem.schedule.Schedule;
 import ghozti.schoolDBsystem.student.Student;
+import ghozti.schoolDBsystem.teacher.Teacher;
+
 import java.util.ArrayList;
 
 public class RemoveByName {
 
-    public void removeByName(ArrayList<Student> list, String target){
+    public void removeByName(ArrayList<Student> list, String target) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getName().equals(target)) {
+                list.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void removeByName(ArrayList<Teacher> list, String target){
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getName().equals(target)){
+                list.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void removeByName(ArrayList<Schedule> list, String target){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getID().equals(target)){
+                list.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void removeByName(ArrayList<Class> list, String target){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getID().equals(target)){
                 list.remove(i);
                 return;
             }
