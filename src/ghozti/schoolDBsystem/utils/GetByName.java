37526f4f33a78cc608f371d84df04a2d2382ac4f
Ghozti <ybalderas1005@gmail.com>
2021-04-16 14:15:45 -0400

    - realized my mistake

--- src/ghozti/schoolDBsystem/utils/GetByName.java
+++ src/ghozti/schoolDBsystem/utils/GetByName.java
@@ -1,6 +1,36 @@
 package ghozti.schoolDBsystem.utils;
 
+import ghozti.schoolDBsystem.classes.Class;
+import ghozti.schoolDBsystem.student.Student;
+import ghozti.schoolDBsystem.teacher.Teacher;
+import java.util.ArrayList;
+
 public class GetByName {
 
-    public
+    public Student getByNameS(ArrayList<Student> studentArrayList, String target){
+        for (int i = 0; i < studentArrayList.size(); i++) {
+            if (target.equals(studentArrayList.get(i).getName())){
+                return studentArrayList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public Teacher getByNameT(ArrayList<Teacher> teacherArrayList, String target){
+        for (int i = 0; i < teacherArrayList.size(); i++) {
+            if (target.equals(teacherArrayList.get(i).getName())){
+                return teacherArrayList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public Class getByNameC(ArrayList<Class> classArrayList, String target){
+        for (int i = 0; i < classArrayList.size(); i++) {
+            if (target.equals(classArrayList.get(i).getSubject())){
+                return classArrayList.get(i);
+            }
+        }
+        return null;
+    }
 }
