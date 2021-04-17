297f8ced36eac7fca405953c656a79d26b5943c5
Ghozti <ybalderas1005@gmail.com>
2021-04-17 14:34:20 -0400

    - added ID to Student

--- src/ghozti/schoolDBsystem/student/Student.java
+++ src/ghozti/schoolDBsystem/student/Student.java
@@ -5,17 +5,18 @@ public class Student {
     private String name;//name of student
     private int age;//age of student
     private String birthDate;//birthdate of student
+    private String ID;//ID
     private double GPA;//GPA of student
 
     //constructor #1 will allow the user to set the fields except GPA
-    public Student(String name,int age, String birthDate){
+    public Student(String name,int age, String birthDate,String ID){
         this.name = name;
         this.age = age;
         this.birthDate = birthDate;
     }
 
     //constructor #2 will allow the user to set all fields
-    public Student(String name,int age, String birthDate,double GPA){
+    public Student(String name,int age, String birthDate,String ID,double GPA){
         this.name = name;
         this.age = age;
         this.birthDate = birthDate;
--- src/ghozti/schoolDBsystem/utils/DBgetters/GetByID.java
+++ src/ghozti/schoolDBsystem/utils/DBgetters/GetByID.java
@@ -1,4 +1,37 @@
 package ghozti.schoolDBsystem.utils.DBgetters;
 
+import ghozti.schoolDBsystem.classes.Class;
+import ghozti.schoolDBsystem.student.Student;
+import ghozti.schoolDBsystem.teacher.Teacher;
+
+import java.util.ArrayList;
+
 public class GetByID {
+
+    public Student getByIDS(ArrayList<Student> studentArrayList, String target){
+        for (int i = 0; i < studentArrayList.size(); i++) {
+            if (target.equals(studentArrayList.get(i).ge)){
+                return studentArrayList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public Teacher getByIDT(ArrayList<Teacher> teacherArrayList, String target){
+        for (int i = 0; i < teacherArrayList.size(); i++) {
+            if (target.equals(teacherArrayList.get(i).getName())){
+                return teacherArrayList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public Class getByIDC(ArrayList<Class> classArrayList, String target){
+        for (int i = 0; i < classArrayList.size(); i++) {
+            if (target.equals(classArrayList.get(i).getSubject())){
+                return classArrayList.get(i);
+            }
+        }
+        return null;
+    }
 }
