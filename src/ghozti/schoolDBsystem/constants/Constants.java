4f9051bd67deddf13a0ed220fb3497540d7fe25b
Ghozti <ybalderas1005@gmail.com>
2021-04-18 21:21:37 -0400

    - added constants

--- src/ghozti/schoolDBsystem/constants/Constants.java
+++ src/ghozti/schoolDBsystem/constants/Constants.java
@@ -2,11 +2,15 @@ package ghozti.schoolDBsystem.constants;
 
 public class Constants {
 
-    public static class Dialouge{
-
+    public static class Dialouge extends Constants{
+        public static final String setname  = "Enter a name: ";
+        public static final String setPeriod = "Enter a period";
+        public static final String generatingID = "Generating ID...";
+        public static final String setBday = "Enter a birth date: ";
+        public static final String setSubject = "Enter a subject: ";
     }
 
-    public static class Values{
+    public static class Values extends Constants{
 
     }
 }
