fa8226f9c7dfa53e1a792ae024b466b321ce3f8d
Ghozti <ybalderas1005@gmail.com>
2021-04-21 16:01:19 -0400

    - finished RemoveByID

--- src/ghozti/schoolDBsystem/utils/algorithms/DBremovers/RemoveByID.java
+++ src/ghozti/schoolDBsystem/utils/algorithms/DBremovers/RemoveByID.java
@@ -1,4 +1,42 @@
 package ghozti.schoolDBsystem.utils.algorithms.DBremovers;
 
+import ghozti.schoolDBsystem.classes.Class;
+import ghozti.schoolDBsystem.schedule.Schedule;
+import ghozti.schoolDBsystem.student.Student;
+import ghozti.schoolDBsystem.teacher.Teacher;
+import java.util.ArrayList;
+
 public class RemoveByID {
+
+    public void removeS(ArrayList<Student> list, String ID){
+        for (int i = 0; i < list.size(); i++) {
+            if (ID.equals(list.get(i).getID())){
+                list.remove(i);
+            }
+        }
+    }
+
+    public void removeT(ArrayList<Teacher> list, String ID){
+        for (int i = 0; i < list.size(); i++) {
+            if (ID.equals(list.get(i).getID())){
+                list.remove(i);
+            }
+        }
+    }
+
+    public void removeC(ArrayList<Class> list, String ID){
+        for (int i = 0; i < list.size(); i++) {
+            if (ID.equals(list.get(i).getID())){
+                list.remove(i);
+            }
+        }
+    }
+
+    public void removeSC(ArrayList<Schedule> list, String ID){
+        for (int i = 0; i < list.size(); i++) {
+            if (ID.equals(list.get(i).getID())){
+                list.remove(i);
+            }
+        }
+    }
 }
