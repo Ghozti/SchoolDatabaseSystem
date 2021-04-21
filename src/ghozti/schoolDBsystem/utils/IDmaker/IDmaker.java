7ff745eb25122c7f4b31f285b2c41df411096adf
Ghozti <ybalderas1005@gmail.com>
2021-04-20 20:58:21 -0400

    - commented on IDmaker

--- src/ghozti/schoolDBsystem/utils/IDmaker/IDmaker.java
+++ src/ghozti/schoolDBsystem/utils/IDmaker/IDmaker.java
@@ -4,6 +4,13 @@ import java.util.Random;
 
 public class IDmaker {
 
+    /**
+     * This method:
+     * will take in a random number 12 times using rand.nextInt()
+     * then it will add that number to the array.
+     * then it will combine the array elements into a string
+     */
+
     public String idGenerator(){
         Random rand = new Random();
         StringBuilder builder = new StringBuilder();
