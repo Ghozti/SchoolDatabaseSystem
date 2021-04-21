3f497d3652d0cc74c02ca9d78a81518258a19756
Ghozti <ybalderas1005@gmail.com>
2021-04-21 11:16:00 -0400

    - commented on GetByID

--- src/ghozti/schoolDBsystem/utils/algorithms/DBgetters/GetByID.java
+++ src/ghozti/schoolDBsystem/utils/algorithms/DBgetters/GetByID.java
@@ -7,6 +7,15 @@ import java.util.ArrayList;
 
 public class GetByID {
 
+    /**
+     * These methods are used to retrieve an object in the DB array lists based on their ID
+     * getByIDS - for students
+     * getByIDS - for teachers
+     * getByIDC - for classes
+     *
+     * this is the most recommend method to use since it is accurate to retrieving the specified object.
+     */
+
     public Student getByIDS(ArrayList<Student> studentArrayList, String target){
         for (int i = 0; i < studentArrayList.size(); i++) {
             if (target.equals(studentArrayList.get(i).getID())){
